39bf137ee43ce474e63cffc6642692966bb38b63
romeocontrol <romeocontrol@googlemail.com>
2024-04-18 11:31:52 +0200

    Update owserver.sh

--- rootfs/etc/cont-init.d/owserver.sh
+++ rootfs/etc/cont-init.d/owserver.sh
@@ -1,36 +1,3 @@
 #!/command/with-contenv bashio
 
-declare device
-declare device_type
-declare temperature_scale
-device=$(bashio::config 'device')
-device_type=$(bashio::config 'device_type')
-temperature_scale=$(bashio::config 'temperature_scale')
 
-if  bashio::var.equals "${device_type}" "fake"; then
-    bashio::log.info "Configuring fake device"
-    sed -i "s/%%device%%/FAKE = DS18B20,DS2405/g" /etc/owfs.conf
-elif bashio::var.equals "${device_type}" "usb"; then
-    bashio::log.info "Configuring usb device"
-    sed -i "s/%%device%%/usb = all/g" /etc/owfs.conf
-elif bashio::var.equals "${device_type}" "ha7net"; then
-    if  bashio::config.exists "ha7net_server"; then
-        bashio::log.info "Configuring ha7net device"
-        sed -i "s/%%device%%/ha7net = $(bashio::config 'ha7net_server')/g" /etc/owfs.conf
-    fi    
-elif bashio::var.equals "${device_type}" "ednet"; then
-    if  bashio::config.exists "ednet_server"; then
-        bashio::log.info "Configuring ednet device"
-        sed -i "s/%%device%%/ednet = $(bashio::config 'ednet_server')/g" /etc/owfs.conf        
-    else
-        bashio::log.error "No ednet server provided, using FAKE device instead!"
-        sed -i "s/%%device%%/FAKE = DS18B20,DS2405/g" /etc/owfs.conf
-    fi
-else
-    bashio::log.info "Configuring ${device} device"
-    device=$(bashio::string.replace ${device} '/' '\/')
-    sed -i "s/%%device%%/device = ${device}/g" /etc/owfs.conf
-fi
-
-bashio::log.info "Configuring temperature scale: ${temperature_scale}"
-sed -i "s/%%temperature_scale%%/${temperature_scale}/g" /etc/owfs.conf
